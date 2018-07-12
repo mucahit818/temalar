2c1b29d61c0bd04d7f15568390fa40c3908c69a2
mucahit818 <41131878+mucahit818@users.noreply.github.com>
2018-07-13 02:12:37 +0300

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,586 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "300", //'700' or 'responsive'
+    background_color: "#a81818",
+    background_transparent: "yes",
+    border_color: "#a81818",
+    
+    //State defaults
+    state_description: "Population",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "0": {},
+    TUR2229: {
+      name: "Aydin",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/aydin.html",
+      description: "2017 official population: 1.080.839"
+    },
+    TUR2230: {
+      name: "Izmir",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/izmir.html",
+      description: "2017 official population: 4.279.677"
+    },
+    TUR2238: {
+      name: "Balikesir",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2239: {
+      name: "Çanakkale",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/canakkale.html",
+      description: "2017 official population: 530.417"
+    },
+    TUR2240: {
+      name: "Edirne",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2241: {
+      name: "Kirklareli",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2242: {
+      name: "Tekirdag",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2263: {
+      name: "Bilecik",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2264: {
+      name: "Bursa",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/bursa.html",
+      description: "2017 official population: 2.936.803"
+    },
+    TUR2265: {
+      name: "Istanbul",
+      url: "https://gezimenya.blogspot.com/p/istanbul.html",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      description: "2017 official population: 15.029.231"
+    },
+    TUR2266: {
+      name: "Kocaeli",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2267: {
+      name: "Sakarya",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2268: {
+      name: "Bolu",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2269: {
+      name: "Eskisehir",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2270: {
+      name: "Kastamonu",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2271: {
+      name: "Antalya",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/antalya.html",
+      description: "2017 official population: 2.364.396"
+    },
+    TUR2272: {
+      name: "Afyonkarahisar",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2273: {
+      name: "Burdur",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2274: {
+      name: "Denizli",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/denizli.html",
+      description: "2017 official population: 1.018.735"
+    },
+    TUR2275: {
+      name: "Isparta",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2276: {
+      name: "Kütahya",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2277: {
+      name: "Manisa",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2278: {
+      name: "Mugla",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/mugla.html",
+      description: "2017 official population: 938.751"
+    },
+    TUR2281: {
+      name: "Adiyaman",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2282: {
+      name: "Elazig",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2283: {
+      name: "K. Maras",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2284: {
+      name: "Malatya",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2285: {
+      name: "Mersin",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2286: {
+      name: "Kirsehir",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2287: {
+      name: "Kayseri",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2288: {
+      name: "Nevsehir",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/nevsehir.html",
+      description: "2017 official population: 292.365"
+    },
+    TUR2289: {
+      name: "Hatay",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2290: {
+      name: "Amasya",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2291: {
+      name: "Çorum",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2292: {
+      name: "Giresun",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2293: {
+      name: "Ordu",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2294: {
+      name: "Sinop",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2295: {
+      name: "Sivas",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2296: {
+      name: "Samsun",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2297: {
+      name: "Tokat",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2298: {
+      name: "Artvin",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2299: {
+      name: "Erzurum",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2300: {
+      name: "Erzincan",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2301: {
+      name: "Rize",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2302: {
+      name: "Trabzon",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/trabzon.html",
+      description: "2017 official population: 786.326"
+    },
+    TUR2307: {
+      name: "Agri",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2308: {
+      name: "Bingöl",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2309: {
+      name: "Diyarbakir",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2310: {
+      name: "Mus",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR2311: {
+      name: "Bitlis",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3008: {
+      name: "Ankara",
+      url: "https://gezimenya.blogspot.com/p/ankara.html",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      description: "2017 official population: 5.445.026"
+    },
+    TUR3009: {
+      name: "Çankiri",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3010: {
+      name: "Zinguldak",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3011: {
+      name: "Konya",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3012: {
+      name: "Karaman",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3014: {
+      name: "Usak",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3016: {
+      name: "Gaziantep",
+      color: "#ff0000",
+      hover_color: "#a81818",
+      url: "https://gezimenya.blogspot.com/p/gaziantep.html",
+      description: "2017 official population: 2.005.515"
+    },
+    TUR3017: {
+      name: "Sanliurfa",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3018: {
+      name: "Adana",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3019: {
+      name: "Kinkkale",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3020: {
+      name: "Nigde",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3021: {
+      name: "Aksaray",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3026: {
+      name: "Yozgat",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3031: {
+      name: "Gümüshane",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3032: {
+      name: "Bayburt",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3040: {
+      name: "Kars",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3041: {
+      name: "Mardin",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3042: {
+      name: "Batman",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3043: {
+      name: "Siirt",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3044: {
+      name: "Sirnak",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3045: {
+      name: "Tunceli",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3047: {
+      name: "Hakkari",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR3048: {
+      name: "Van",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR4839: {
+      name: "Ardahan",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR4840: {
+      name: "Iğdir",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR4841: {
+      name: "Kilis",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR4842: {
+      name: "Osmaniye",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR5518: {
+      name: "Yalova",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR5519: {
+      name: "Düzce",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR5520: {
+      name: "Karabük",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    },
+    TUR5521: {
+      name: "Bartın",
+      color: "#cccccc",
+      hover_color: "#19191b",
+      description: "In the future / Gelekte"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "39.911652",
+      lng: "32.840305",
+      name: "Ankara",
+      color: "#ffffff",
+      description: "capital"
+    }
+  },
+  labels: {},
+  regions: {}
+};
